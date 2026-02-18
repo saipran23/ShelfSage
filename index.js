82a1265e53abd66e915e76b6cf65385eb50ee98a
saipran23 <87567651+saipran23@users.noreply.github.com>
2026-02-18 11:27:35 +0530

    Change database password to a placeholder
    Updated database password to a placeholder.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "OpenShelf",
-  password: "Saipraneeth@2006",
+  password: "----",
   port: 5432,
 });
 db.connect();
